05de864140e8033b64e6fb7962af5f08ce1449a0
ucjonathan <jonathan@ultracart.com>
2016-08-31 11:46:26 -0400

    Minor update

--- com_ultracart_admin_v2/configuration.py
+++ com_ultracart_admin_v2/configuration.py
@@ -61,7 +61,7 @@ class Configuration(object):
         Constructor
         """
         # Default Base url
-        self.host = "https://localhost/rest/admin/v2"
+        self.host = "https://secure.ultracart.com/rest/admin/v2"
         # Default api client
         self.api_client = None
         # Temp file folder for downloading files
--- docs/ItemApi.md
+++ docs/ItemApi.md
@@ -1,6 +1,6 @@
 # com_ultracart_admin_v2.ItemApi
 
-All URIs are relative to *https://localhost/rest/admin/v2*
+All URIs are relative to *https://secure.ultracart.com/rest/admin/v2*
 
 Method | HTTP request | Description
 ------------- | ------------- | -------------
