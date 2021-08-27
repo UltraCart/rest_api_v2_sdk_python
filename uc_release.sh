508eb1e8e17da16562ec4a0b02bd8fcd473aa4b4
Perry Tew <perry@ultracart.com>
2021-08-27 11:05:24 -0400

    customer profile affiliate info now contains aff first and last name

--- uc_release.sh
+++ uc_release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git tag 3.6.6
+git tag 3.6.7
--- ultracart/models/customer_affiliate.py
+++ ultracart/models/customer_affiliate.py
@@ -32,25 +32,35 @@ class CustomerAffiliate(object):
     """
     swagger_types = {
         'affiliate_oid': 'int',
-        'email': 'str'
+        'email': 'str',
+        'first_name': 'str',
+        'last_name': 'str'
     }
 
     attribute_map = {
         'affiliate_oid': 'affiliate_oid',
-        'email': 'email'
+        'email': 'email',
+        'first_name': 'first_name',
+        'last_name': 'last_name'
     }
 
-    def __init__(self, affiliate_oid=None, email=None):  # noqa: E501
+    def __init__(self, affiliate_oid=None, email=None, first_name=None, last_name=None):  # noqa: E501
         """CustomerAffiliate - a model defined in Swagger"""  # noqa: E501
 
         self._affiliate_oid = None
         self._email = None
+        self._first_name = None
+        self._last_name = None
         self.discriminator = None
 
         if affiliate_oid is not None:
             self.affiliate_oid = affiliate_oid
         if email is not None:
             self.email = email
+        if first_name is not None:
+            self.first_name = first_name
+        if last_name is not None:
+            self.last_name = last_name
 
     @property
     def affiliate_oid(self):
@@ -97,6 +107,52 @@ class CustomerAffiliate(object):
         """
 
         self._email = email
+
+    @property
+    def first_name(self):
+        """Gets the first_name of this CustomerAffiliate.  # noqa: E501
+
+        First name  # noqa: E501
+
+        :return: The first_name of this CustomerAffiliate.  # noqa: E501
+        :rtype: str
+        """
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, first_name):
+        """Sets the first_name of this CustomerAffiliate.
+
+        First name  # noqa: E501
+
+        :param first_name: The first_name of this CustomerAffiliate.  # noqa: E501
+        :type: str
+        """
+
+        self._first_name = first_name
+
+    @property
+    def last_name(self):
+        """Gets the last_name of this CustomerAffiliate.  # noqa: E501
+
+        Last name  # noqa: E501
+
+        :return: The last_name of this CustomerAffiliate.  # noqa: E501
+        :rtype: str
+        """
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, last_name):
+        """Sets the last_name of this CustomerAffiliate.
+
+        Last name  # noqa: E501
+
+        :param last_name: The last_name of this CustomerAffiliate.  # noqa: E501
+        :type: str
+        """
+
+        self._last_name = last_name
 
     def to_dict(self):
         """Returns the model properties as a dict"""
