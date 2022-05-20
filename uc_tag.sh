99c6413efc052743a886d044efd0040a37987c8b
Perry Tew <perry@ultracart.com>
2022-05-20 19:29:22 -0400

    dependabot on java gson

--- uc_tag.sh
+++ uc_tag.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git tag 3.10.3
+git tag 3.10.4
--- ultracart/configuration.py
+++ ultracart/configuration.py
@@ -266,5 +266,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2.0.0\n"\
-               "SDK Package Version: 3.10.3".\
+               "SDK Package Version: 3.10.4".\
                format(env=sys.platform, pyversion=sys.version)
