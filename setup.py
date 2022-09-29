059730a27c924cb62206266c1c319f290542de1d
Perry Tew <perry@ultracart.com>
2022-09-29 15:11:43 -0400

    automation test

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "ultracart_rest_sdk"
-VERSION = "3.10.52"
+VERSION = "3.10.53"
 # To install the library, run the following
 #
 # python setup.py install
