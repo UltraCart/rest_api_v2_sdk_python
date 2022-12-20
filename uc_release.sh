cacb6e8a6a6c0566eb896b210ee6c052bdae183e
Perry Tew <perry@ultracart.com>
2022-12-20 12:07:53 -0500

    added createEmailSendingDomain2 to support additional MTAs

--- uc_release.sh
+++ uc_release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git tag 4.0.100-RC
+git tag 4.0.101-RC
--- ultracart/__init__.py
+++ ultracart/__init__.py
@@ -11,7 +11,7 @@
 """
 
 
-__version__ = "4.0.100-RC"
+__version__ = "4.0.101-RC"
 
 # import ApiClient
 from ultracart.api_client import ApiClient
