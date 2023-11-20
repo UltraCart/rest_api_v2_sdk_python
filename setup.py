f8ce5f0cb9ea48585710bae627ddcfa4be87c8cf
Perry Tew <perry@ultracart.com>
2023-11-20 11:40:59 -0500

    automation test

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "ultracart_rest_sdk"
-VERSION = "3.10.173"
+VERSION = "3.10.174"
 # To install the library, run the following
 #
 # python setup.py install
--- ultracart/models/coupon_percent_off_items_and_free_shipping.py
+++ ultracart/models/coupon_percent_off_items_and_free_shipping.py
@@ -32,28 +32,38 @@ class CouponPercentOffItemsAndFreeShipping(object):
     """
     swagger_types = {
         'discount_percent': 'float',
+        'excluded_item_tags': 'list[str]',
         'excluded_items': 'list[str]',
+        'item_tags': 'list[str]',
         'items': 'list[str]'
     }
 
     attribute_map = {
         'discount_percent': 'discount_percent',
+        'excluded_item_tags': 'excluded_item_tags',
         'excluded_items': 'excluded_items',
+        'item_tags': 'item_tags',
         'items': 'items'
     }
 
-    def __init__(self, discount_percent=None, excluded_items=None, items=None):  # noqa: E501
+    def __init__(self, discount_percent=None, excluded_item_tags=None, excluded_items=None, item_tags=None, items=None):  # noqa: E501
         """CouponPercentOffItemsAndFreeShipping - a model defined in Swagger"""  # noqa: E501
 
         self._discount_percent = None
+        self._excluded_item_tags = None
         self._excluded_items = None
+        self._item_tags = None
         self._items = None
         self.discriminator = None
 
         if discount_percent is not None:
             self.discount_percent = discount_percent
+        if excluded_item_tags is not None:
+            self.excluded_item_tags = excluded_item_tags
         if excluded_items is not None:
             self.excluded_items = excluded_items
+        if item_tags is not None:
+            self.item_tags = item_tags
         if items is not None:
             self.items = items
 
@@ -81,6 +91,29 @@ class CouponPercentOffItemsAndFreeShipping(object):
         self._discount_percent = discount_percent
 
     @property
+    def excluded_item_tags(self):
+        """Gets the excluded_item_tags of this CouponPercentOffItemsAndFreeShipping.  # noqa: E501
+
+        A list of item tags which cannot be discounted.  # noqa: E501
+
+        :return: The excluded_item_tags of this CouponPercentOffItemsAndFreeShipping.  # noqa: E501
+        :rtype: list[str]
+        """
+        return self._excluded_item_tags
+
+    @excluded_item_tags.setter
+    def excluded_item_tags(self, excluded_item_tags):
+        """Sets the excluded_item_tags of this CouponPercentOffItemsAndFreeShipping.
+
+        A list of item tags which cannot be discounted.  # noqa: E501
+
+        :param excluded_item_tags: The excluded_item_tags of this CouponPercentOffItemsAndFreeShipping.  # noqa: E501
+        :type: list[str]
+        """
+
+        self._excluded_item_tags = excluded_item_tags
+
+    @property
     def excluded_items(self):
         """Gets the excluded_items of this CouponPercentOffItemsAndFreeShipping.  # noqa: E501
 
@@ -102,6 +135,29 @@ class CouponPercentOffItemsAndFreeShipping(object):
         """
 
         self._excluded_items = excluded_items
+
+    @property
+    def item_tags(self):
+        """Gets the item_tags of this CouponPercentOffItemsAndFreeShipping.  # noqa: E501
+
+        An optional list of item tags which will receive a discount.  If blank, discount applies to all items except excluded items.  # noqa: E501
+
+        :return: The item_tags of this CouponPercentOffItemsAndFreeShipping.  # noqa: E501
+        :rtype: list[str]
+        """
+        return self._item_tags
+
+    @item_tags.setter
+    def item_tags(self, item_tags):
+        """Sets the item_tags of this CouponPercentOffItemsAndFreeShipping.
+
+        An optional list of item tags which will receive a discount.  If blank, discount applies to all items except excluded items.  # noqa: E501
+
+        :param item_tags: The item_tags of this CouponPercentOffItemsAndFreeShipping.  # noqa: E501
+        :type: list[str]
+        """
+
+        self._item_tags = item_tags
 
     @property
     def items(self):
