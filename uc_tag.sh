f71d77dfe95cb491790530bd302890e4f4562899
Perry Tew <perry@ultracart.com>
2025-10-20 16:34:53 -0400

    conversation api bug fix for bad url on agent profile kb upload

--- uc_tag.sh
+++ uc_tag.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git tag 4.1.24
+git tag 4.1.25
--- ultracart/__init__.py
+++ ultracart/__init__.py
@@ -11,7 +11,7 @@
 """
 
 
-__version__ = "4.1.24"
+__version__ = "4.1.25"
 
 # import ApiClient
 from ultracart.api_client import ApiClient
