4643de97785f90f04d232b05d1079b7e063ee639
Perry Tew <perry@ultracart.com>
2026-02-02 13:14:20 -0500

    conversations - add default phone number to the agent auth

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "ultracart_rest_sdk"
-VERSION = "3.11.55"
+VERSION = "3.11.56"
 # To install the library, run the following
 #
 # python setup.py install
--- uc_tag.sh
+++ uc_tag.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git tag 3.11.55
+git tag 3.11.56
