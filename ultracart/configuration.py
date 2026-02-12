6beb278168654ff67ffd40418472f252127ca8a9
Perry Tew <perry@ultracart.com>
2026-02-12 16:04:47 -0500

    build automation run, no code changes

--- ultracart/configuration.py
+++ ultracart/configuration.py
@@ -266,5 +266,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2.0.0\n"\
-               "SDK Package Version: 3.11.61".\
+               "SDK Package Version: 3.11.62".\
                format(env=sys.platform, pyversion=sys.version)
