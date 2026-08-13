f4cc8aff75d356867331f3df384e09dc5906637a
Perry Tew <perry@ultracart.com>
2026-08-13 13:23:20 -0400

    added arbitrary discounts to order creation for historical imports

--- uc_release.sh
+++ uc_release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git tag 4.1.126
+git tag 4.1.127
--- ultracart/model/order_summary.py
+++ ultracart/model/order_summary.py
@@ -96,6 +96,8 @@ class OrderSummary(ModelNormal):
             'actual_profit_review': (bool,),  # noqa: E501
             'actual_shipping': (Currency,),  # noqa: E501
             'arbitrary_shipping_handling_total': (Currency,),  # noqa: E501
+            'arbitrary_subtotal_discount': (Currency,),  # noqa: E501
+            'arbitrary_taxable_subtotal_discount': (Currency,),  # noqa: E501
             'health_benefit_card_amount': (Currency,),  # noqa: E501
             'health_benefit_card_refunded': (Currency,),  # noqa: E501
             'internal_gift_certificate_amount': (Currency,),  # noqa: E501
@@ -130,6 +132,8 @@ class OrderSummary(ModelNormal):
         'actual_profit_review': 'actual_profit_review',  # noqa: E501
         'actual_shipping': 'actual_shipping',  # noqa: E501
         'arbitrary_shipping_handling_total': 'arbitrary_shipping_handling_total',  # noqa: E501
+        'arbitrary_subtotal_discount': 'arbitrary_subtotal_discount',  # noqa: E501
+        'arbitrary_taxable_subtotal_discount': 'arbitrary_taxable_subtotal_discount',  # noqa: E501
         'health_benefit_card_amount': 'health_benefit_card_amount',  # noqa: E501
         'health_benefit_card_refunded': 'health_benefit_card_refunded',  # noqa: E501
         'internal_gift_certificate_amount': 'internal_gift_certificate_amount',  # noqa: E501
@@ -199,6 +203,8 @@ class OrderSummary(ModelNormal):
             actual_profit_review (bool): Actual profit needs review. [optional]  # noqa: E501
             actual_shipping (Currency): [optional]  # noqa: E501
             arbitrary_shipping_handling_total (Currency): [optional]  # noqa: E501
+            arbitrary_subtotal_discount (Currency): [optional]  # noqa: E501
+            arbitrary_taxable_subtotal_discount (Currency): [optional]  # noqa: E501
             health_benefit_card_amount (Currency): [optional]  # noqa: E501
             health_benefit_card_refunded (Currency): [optional]  # noqa: E501
             internal_gift_certificate_amount (Currency): [optional]  # noqa: E501
@@ -310,6 +316,8 @@ class OrderSummary(ModelNormal):
             actual_profit_review (bool): Actual profit needs review. [optional]  # noqa: E501
             actual_shipping (Currency): [optional]  # noqa: E501
             arbitrary_shipping_handling_total (Currency): [optional]  # noqa: E501
+            arbitrary_subtotal_discount (Currency): [optional]  # noqa: E501
+            arbitrary_taxable_subtotal_discount (Currency): [optional]  # noqa: E501
             health_benefit_card_amount (Currency): [optional]  # noqa: E501
             health_benefit_card_refunded (Currency): [optional]  # noqa: E501
             internal_gift_certificate_amount (Currency): [optional]  # noqa: E501
