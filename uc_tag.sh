9fd642749824d93f14abdda4d838ff018322a50f
Perry Tew <perry@ultracart.com>
2026-08-18 11:49:27 -0400

    customer activity now returns the SF communications active flag and last_activity_dts

--- uc_tag.sh
+++ uc_tag.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git tag 4.1.130
+git tag 4.1.131
--- ultracart/__init__.py
+++ ultracart/__init__.py
@@ -11,7 +11,7 @@
 """
 
 
-__version__ = "4.1.130"
+__version__ = "4.1.131"
 
 # import ApiClient
 from ultracart.api_client import ApiClient
