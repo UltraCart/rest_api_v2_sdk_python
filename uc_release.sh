2286995b8ec207031af8c20dd11e68a9df8c6c13
Perry Tew <perry@ultracart.com>
2026-09-18 16:12:02 -0400

    sdk automation testing

--- uc_release.sh
+++ uc_release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git tag 4.1.161
+git tag 4.1.162
--- ultracart/__init__.py
+++ ultracart/__init__.py
@@ -11,7 +11,7 @@
 """
 
 
-__version__ = "4.1.161"
+__version__ = "4.1.162"
 
 # import ApiClient
 from ultracart.api_client import ApiClient
